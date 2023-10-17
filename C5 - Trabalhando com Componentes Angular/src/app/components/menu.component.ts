2faa7f09003a46e2a9502ac5489b5fc734938c68
nathy10carvalh0 <nathy10carvalho@gmail.com>
2023-10-17 15:48:07 -0300

    atualizando pastas

---
+++ C5 - Trabalhando com Componentes Angular/src/app/components/menu.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'menu-componente',
+    template: '<h2>Felipe</h2>',
+    styles: ['h2{color: blue}'],
+
+})
+export class MenuComponent {}
---
+++ C6 - LifeCycle Hooks Angular/life-cicle/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'life-cicle';
+  isAliveCheckSample:boolean = true
+
+  disposeCheckSample():void{
+    this.isAliveCheckSample = false
+  }
+}
